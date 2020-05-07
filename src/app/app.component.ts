6f9da635c9445c9aa605fd965bdfbd655714d6c4
Chris Carpenter <chris@uberchris.com>
2020-05-06 20:14:23 -0500

    implement file upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BlobServiceClient, BlockBlobParallelUploadOptions, BlobUploadCommonResponse } from '@azure/storage-blob';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form-reader-demo';
+  storageAccount: string;
+  sasToken: string;
+  storageContainer: string;
+  file: File;
+  fileUri: string = null;
+
+  selectFile(event) {
+    const fileList: FileList = event.target.files;
+    this.file = fileList[0];
+  }
+
+  async uploadFile() {
+    const blobServiceClient = new BlobServiceClient(
+      `https://${this.storageAccount}.blob.core.windows.net${this.sasToken}`
+    );
+    const containerClient = blobServiceClient.getContainerClient(this.storageContainer);
+    const blockBlobClient = containerClient.getBlockBlobClient(this.file.name);
+    const options: BlockBlobParallelUploadOptions = { blobHTTPHeaders: { blobContentType: this.file.type } };
+    await blockBlobClient.uploadBrowserData(this.file, options);
+    this.fileUri = `https://${this.storageAccount}.blob.core.windows.net/${this.storageContainer}/${this.file.name}${this.sasToken}`
+  }
 }
